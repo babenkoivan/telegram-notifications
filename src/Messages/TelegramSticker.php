350b73916a9a768898e1dcbb10bed8368b1ab268
ivanbabenko <babenko.i.a@gmail.com>
2017-02-19 23:22:43 +0300

    Added stickers support

---
+++ src/Messages/TelegramSticker.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TelegramNotifications\Messages;
+
+class TelegramSticker extends TelegramEntity
+{
+    protected $required = [
+        'sticker'
+    ];
+
+    protected $optional = [
+        'disable_notification',
+    ];
+}
